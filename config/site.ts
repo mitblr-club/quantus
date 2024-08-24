cd23812ae0d5b96c2e045d9cec3258533a4de723
Archit Chadalawada <drarchitch@gmail.com>
2024-08-24 18:51:27 +0530

    Update site.ts (QuantUs)

--- config/site.ts
+++ config/site.ts
@@ -1,36 +1,36 @@
-import { NavItem } from '@/types/nav-item';
+import { NavItem } from "@/types/nav-item";
 
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: 'Club Name',
-  institution: 'Manipal Institute of Technology Bengaluru (MIT)',
-  description: "Site metdata description!",
-  eventsTableId: 'f348a5f5bb934f87bbe2f16c10521cb5',
-  blogTableId: 'b161b7c3fb8c415cac57781069d8b4fb',
+  name: "Club Name",
+  institution: "Manipal Institute of Technology Bengaluru (MIT)",
+  description: "Enter the description of your site.",
+  eventsTableId: "df0bbd9604a846468e6aaf7f41042c1c",
+  blogTableId: "d8caac170c6243a18228b8467ec1f41e",
   navLinks: [
     {
-      title: 'Home',
-      href: '/',
+        title: "Home",
+        href: "/",
     },
     {
-      title: 'Blog',
-      href: '/blog',
+        title: "Blog",
+        href: "/blog",
     },
     {
-      title: 'Events',
-      href: '/events',
-    },
+        title: "Events",
+        href: "/events",
+    }
   ] satisfies NavItem[],
   mediaLinks: {
-    instagram: 'https://www.instagram.com/',
-    linkedin: 'https://in.linkedin.com/',
-    github: 'https://github.com/',
-    twitter: 'https://twitter.com/',
-    institute: 'https://manipal.edu/mitblr.html',
+    instagram: "https://www.instagram.com/",
+    linkedin: "https://in.linkedin.com/",
+    github: "https://github.com/",
+    twitter: "https://twitter.com/",
+    institute: "https://manipal.edu/mitblr.html",
   },
   contactDetails: {
-    number: '+91 1234567890',
-    email: 'abc.efg@gmail.com',
-  },
+    number: "+91 1234567890",
+    email: "abc.efg@gmail.com",
+  }
 };
